3d938d1152dc6a1d481999d4dcd5a1a694740630
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-03-24 11:04:06 -0400

    initial commit

---
+++ Encourgementkitty/Encourgementkitty/EncourgementkittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncourgementkittyApp.swift
+//  Encourgementkitty
+//
+//  Created by Soumyadeep Chatterjee on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EncourgementkittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Encourgementkitty/EncourgementkittyTests/EncourgementkittyTests.swift
@@ -0,0 +1,17 @@
+//
+//  EncourgementkittyTests.swift
+//  EncourgementkittyTests
+//
+//  Created by Soumyadeep Chatterjee on 3/24/25.
+//
+
+import Testing
+@testable import Encourgementkitty
+
+struct EncourgementkittyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
